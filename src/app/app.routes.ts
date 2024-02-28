d3fa13074afc82c013a5ab43b74c7404b37fde67
Chris <capacheco.22@est.ucab.edu.ve>
2024-02-28 15:05:07 -0400

    feat: initial commit (routes)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MarketingComponent } from './pages/marketing/marketing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MarketingComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+];
